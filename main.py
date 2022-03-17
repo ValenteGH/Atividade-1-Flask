078b44bafad0502f4616ae760e89dda89f392a63
ValenteGH <101368268+ValenteGH@users.noreply.github.com>
2022-03-17 22:55:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from flask import Flask
+app = Flask('app')
+
+@app.route('/')
+def first_route():
+  return '<h1>Minha primeira rota!</h1>'
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=8080)
